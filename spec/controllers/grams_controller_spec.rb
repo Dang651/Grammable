61bd3f414794dc0e9fea8b37f24259647cfaa791
Dan Garcia <DanGx@outlook.com>
2017-08-20 03:24:02 +0000

    Lesson 17 setup validation of image

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -154,6 +154,8 @@ RSpec.describe GramsController, type: :controller do
     end # it
   end # describe
 
+
+
   describe "grams#create action" do
     it "should require users to be logged in" do
       post :create, params: { gram: { message: "Hello" } }
@@ -164,7 +166,13 @@ RSpec.describe GramsController, type: :controller do
       user = FactoryGirl.create(:user)
       sign_in user
       
-      post :create, params: { gram: { message: 'Hello!' } }
+      post :create, params: { 			
+        gram: {  							
+          message: 'Hello!',
+          picture: fixture_file_upload("/picture.png", 'image/png')
+        }
+      }
+      
       expect(response).to redirect_to root_path
   
       gram = Gram.last
